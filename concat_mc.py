7dd3b0898cdd83187f40faba3e405feef9a8f527
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-09-23 14:37:18 -0400

    Add files via upload

---
+++ concat_mc.py
@@ -0,0 +1,18 @@
+# Dax Dewey
+# 23 SEP 2024
+# Concatenation Check
+
+# Task 1
+first_name = "Dax"
+fav_junk_food = "Donuts"
+
+print(first_name + " likes " + fav_junk_food + ".")
+
+
+# Task 2
+
+print("What ive learnd about concatenation:")
+print("I have learnd that you can put sentinsise tegether with code.")
+print("I also have lernd that you can add more to ")
+
+# Task 3
