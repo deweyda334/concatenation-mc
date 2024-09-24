726b6568c3d888fdfcbba89f12cb471e5850b9fe
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-09-24 14:10:17 -0400

    Add files via upload

--- concat_mc.py
+++ concat_mc.py
@@ -5,7 +5,7 @@
 # Task 1
 first_name = "Dax"
 fav_junk_food = "Donuts"
-
+dollors = "&5.99."
 print(first_name + " likes " + fav_junk_food + ".")
 
 
@@ -16,3 +16,10 @@ print("I have learnd that you can put sentinsise tegether with code.")
 print("I also have lernd that you can add more to ")
 
 # Task 3
+
+price = 3.99
+menu_item = 'chicken sandwich'
+print(f"At Burger King, a {menu_item} costs ${price}.")
+print("At Burger King, " + "a " + menu_item + " costs " + "$" + str(price))
+
+
